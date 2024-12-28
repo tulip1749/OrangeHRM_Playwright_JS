ec7a4de61d9a7edb31c1612342e8a9c63a72cdb5
tulip1749 <tulip1749@gmail.com>
2024-12-28 18:54:31 +0530

    Task 1: Login (#1)
    * implement login feature
    
    * fix syntax issue

---
+++ tests/Login/Login.spec.js
@@ -0,0 +1,13 @@
+const {test,expect} = require ("@playwright/test");
+const {LoginPage} = require('../../PageObjects/LoginPage.js');
+
+test('Login with valid creds', async ({page})=> {
+
+//creating object for LoginPage class
+const loginPage= new LoginPage(page, expect);
+
+await loginPage.websiteLaunch();
+
+await loginPage.validLogin();
+
+})
